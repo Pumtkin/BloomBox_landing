256cf32f0fb7db221d63fd3a31c577011dd836f6
Pumtkinn <pumtkin4@gmail.com>
2025-11-10 17:14:13 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+        {label: 'Store'},
+        {label: 'Seedling'},
+        {label: 'Blossom'},
+        {label: 'Oasis'},
+        {label: 'Planters'},
+        {label: 'Care kits'},
+    ];
